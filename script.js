aac1ce81053959abca731d7f1b80853cc03d37f1
Noopur <noorweb3@outlook.com>
2024-09-19 15:10:46 +0530

    add home page

--- script.js
+++ script.js
@@ -3,8 +3,45 @@ let closeicon = document.querySelector('.close-icon');
 let navlinks = document.querySelector('.navlinks');
 
 function toggleNav() {
-    navlinks.classList.toggle('active');  
-    bar.style.display = bar.style.display === 'none' ? 'block' : 'none'; 
-    closeicon.style.display = closeicon.style.display === 'block' ? 'none' : 'block'; 
+    navlinks.classList.toggle('active');
+
+    if (bar.style.display === 'none') {
+        bar.style.display = 'block';
+        closeicon.style.display = 'none';
+    } else {
+        bar.style.display = 'none';
+        closeicon.style.display = 'block';
+    }
 }
 
+
+let cursor = document.querySelector('.cursor')
+window.addEventListener("mousemove", function (dets) {
+    gsap.to(cursor ,{
+        x:dets.x,
+        y:dets.y,
+        duration:0.6
+    })
+})
+
+
+function Breaktext() {
+    let h1 = document.querySelector('h1')
+let h1text = h1.textContent
+var splittext = h1text.split("")
+var clutter = " "
+splittext.forEach(function (elem) {
+    clutter += `<span>${elem}</span>`
+})
+
+h1.innerHTML = clutter
+}
+Breaktext()
+
+gsap.from("h1 span" , {
+    y:50,
+    duration:0.4,
+    delay:0.1,
+    opacity:0,
+    stagger:0.1,
+})
